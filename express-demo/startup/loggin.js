182e6f196f79dba31ceebdcd7c8ea02a49150ebc
ryongpintu <ryongpintu@gmail.com>
2018-10-04 04:53:52 +0530

    new file

--- express-demo/startup/loggin.js
+++ express-demo/startup/loggin.js
@@ -1,9 +1,7 @@
 require('express-async-errors');
 const winston = require('winston');
-require('winston-mongodb');
 module.exports = function(){
 	winston.add(winston.transports.File,{filename:'logfile.log'});
-	winston.add(winston.transports.MongoDB,{db:'mongodb://localhost/vidlyDB'});
 	winston.handleExceptions(
 	new winston.transports.Console({colorize:true, prettyPrint:true}),
 	new winston.transports.File({filename:'uncoughtException.log'}));
