9e23d95f560566bb0add5f8dc13a625a0a409057
ryongpintu <ryongpintu@gmail.com>
2018-10-04 03:36:54 +0530

    changed the db file

--- express-demo/startup/db.js
+++ express-demo/startup/db.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const mongoose =require('mongoose');
 const config= require('config');
 module.exports=function(){
-	const db=config.get("db");
+	const db=config.get("vidly_db");
 	mongoose.connect(db,{useNewUrlParser: true })
 		.then(()=>winston.info(`Connected to db ${db}`));
 
