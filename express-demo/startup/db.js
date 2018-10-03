600a0785ed32a197e5dc698d7b392c8bbcff40af
ryongpintu <ryongpintu@gmail.com>
2018-10-04 03:53:14 +0530

    Add dvv

--- express-demo/startup/db.js
+++ express-demo/startup/db.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const mongoose =require('mongoose');
 const config= require('config');
 module.exports=function(){
-	const db=config.get("vidly_db");
+	const db=config.get("db");
 	mongoose.connect(db,{useNewUrlParser: true })
 		.then(()=>winston.info(`Connected to db ${db}`));
 
