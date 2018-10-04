56b5215d0924f844971debdbe38d5770fee9ded3
ryongpintu <ryongpintu@gmail.com>
2018-10-04 05:37:16 +0530

    change the package.json

--- express-demo/startup/db.js
+++ express-demo/startup/db.js
@@ -3,7 +3,7 @@ const mongoose =require('mongoose');
 const config= require('config');
 module.exports=function(){
 	const db=config.get("db");
-	mongoose.connect(db,{useNewUrlParser: true })
+	mongoose.connect(db)
 		.then(()=>winston.info(`Connected to db ${db}`));
 
 }
