11d1f6adf7836d91006469b98f589437f5312585
ryongpintu <ryongpintu@gmail.com>
2018-10-26 02:20:29 +0530

    changed package.json file

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const winston = require('winston');
-const path= require('path');
 const exphbs=require('express-handlebars');
 const app = express();
 require('./startup/loggin')();
